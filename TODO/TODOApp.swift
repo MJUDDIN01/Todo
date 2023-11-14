b005ffd8ebd8c7300b52d1197e0de059bbc859f6
MJUDDIN01 <mjuddin88@gmail.com>
2023-11-14 10:13:08 +0000

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Jasim Uddin on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
